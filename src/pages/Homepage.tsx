f35e9fb47ff9ff597b20ccbfa304e6944f0ae23c
Vixs101 <elijahvix695@gmail.com>
2024-07-13 15:37:47 +0100

    Feature: implemented the data fetching and added pagination to the site

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getProducts } from "../services/timbuApi";
 import { LuPhone } from "react-icons/lu";
 import { FaRegEnvelope } from "react-icons/fa";
 import { IoMdHome } from "react-icons/io";
-import { productImage } from "../utils/utils";
+
 import { useNavigate } from "react-router";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+interface Product {
+  id: string;
+  name: string;
+  current_price: number;
+  description: string;
+  photos: any;
+}
 
 function Homepage() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const navigate = useNavigate();
 
   const goToCart = () => {
-    navigate('cart');
+    navigate("cart");
   };
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await getProducts(page, 12);
+        setProducts(response.data.items);
+
+        console.log(response.data.items);
+
+        setTotalPages(Math.ceil(response.data.total / 10));
+
+        console.log(response.data.total / 10);
+      } catch (error) {
+        console.log("Error fetching products: ", error);
+      }
+    };
+
+    fetchProducts();
+  }, [page]);
 
   return (
     <>
-     {/* hero section */}
+      {/* hero section */}
       <section className="flex md:flex-row flex-col px-5 pb-5 pt-14 md:px-20 md:pt-28 md:pb-20 justify-between bg-primary text-white w-full items-center h-full gap-5 md:gap-6 lg:gap-0">
         <div className="flex flex-col items-center md:items-start text-center md:text-start lg:w-2/5 md:h-4/5 gap-5 w-full">
           <h1 className="md:heroText text-3xl md:lg:text-5xl font-semibold">
@@ -41,23 +73,47 @@ function Homepage() {
           Best Selling Items
         </h2>
         <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 h-full mt-5">
-          {productImage.map((product) => (
+          {products.map((product) => (
             <div
               key={product.id}
               className="flex flex-col bg-white p-2 md:p-5 rounded-xl gap-5"
+              style={{ maxHeight: "410px", minHeight: "235px" }}
             >
-              <img src={`/images/${product.pic}.png`} alt={product.alt} />
+              <img
+                src={`https://api.timbu.cloud/images/${product.photos[0].url}`}
+                alt={product.name}
+                className="h-2/3 object-cover"
+              />
 
               <div className="flex justify-between border-t-gray-200 border-t pt-2">
                 <div className="flex flex-col gap-2 text-sm md:text-2xl">
-                  <h3 className="text-primary">{product.title}</h3>
-                  <p>{product.price}</p>
+                  <h3 className="text-primary">{product.name}</h3>
+                  <p>{`$${product.current_price[0].NGN[0]}`}</p>
                 </div>
-                <button className="flex items-center justify-center bg-primary hover:bg-[#8c552f] text-white text-sm md:text-4xl rounded-xl md:p-2 h-10 md:h-auto w-1/4 md:w-1/5 self-center md:self-start" onClick={goToCart}>
-                  {product.button}
+                <button
+                  className="flex items-center justify-center bg-primary hover:bg-[#8c552f] text-white text-sm md:text-4xl rounded-xl md:p-2 h-10 md:h-auto w-1/4 md:w-1/5 self-center md:self-start"
+                  onClick={goToCart}
+                >
+                  <MdOutlineShoppingCart />
                 </button>
               </div>
             </div>
+          ))}
+        </div>
+        {page === totalPages && products.length === 0 && (
+            <h1 className="text-center text-xl font-semibold text-primary">You are all caught up!</h1>
+          )}
+        <div className="flex gap-2 items-center justify-center  mt-5">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index}
+              onClick={() => setPage(index + 1)}
+              className={
+                page == index + 1 ? "text-xl font-semibold text-primary" : ""
+              }
+            >
+              {index}
+            </button>
           ))}
         </div>
       </section>
--- src/services/timbuApi.tsx
+++ src/services/timbuApi.tsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { CgOrganisation } from 'react-icons/cg';
 declare global {
     interface ImportMeta {
       env: {
